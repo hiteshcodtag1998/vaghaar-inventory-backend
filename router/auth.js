9897d3f380461ee864eb6568e09693238144b318
StudentHitesh <hitesh1998.student@gmail.com>
2025-06-08 11:56:51 +0530

    Feat: Added middleware in auth route

--- router/auth.js
+++ router/auth.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const { login, register, getMe, logout } = require('../controller/auth');
+const authenticateJWT = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.post('/login', login);
 
-router.get('/me', getMe);
+router.get('/me', authenticateJWT, getMe);
 
 router.post('/register', register);
 
-router.post('/logout', logout);
+router.get('/logout', authenticateJWT, logout);
 
 module.exports = router;
